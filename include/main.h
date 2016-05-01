2c9b5adbda78506f2761c69dd4f51aefc4d3ae29
Nandakishore K <nandakishorek17@gmail.com>
2016-04-30 22:17:25 -0400

    Fixed: INF value bug

--- include/main.h
+++ include/main.h
@@ -6,5 +6,5 @@
 enum ctrlcode_t {AUTHOR, INIT, ROUTING_TABLE, UPDATE, CRASH, SENDFILE, SENDFILE_STATS, LAST_DATA_PACKET, PENULTIMATE_DATA_PACKET};
 
 // infinity value - Largest unsigned integer that can be represented in 2 bytes
-const uint16_t INF = 0xFF;
+const uint16_t INF = 0xFFFF;
 #endif
